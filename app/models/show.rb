89d1bea68e80f4e3b39b3bd2a027e150e7c78665
ZIming Mao <ziming.mao@yale.edu>
2020-06-05 21:27:32 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
   
+    has_many :characters
+    belongs_to :network
+
+    def actors_list
+        self.characters.map {|character| character.actor.full_name}
+    end
+    
+    def build_network(call_letters:)
+        n = Network.new(call_letters: call_letters)
+        n.shows << self
+        n
+    end
 end
